efe60f6c398ba0af7f161facbeea89460ebb78f1
MaxPolak97 <maxpolak97@gmail.com>
2025-08-16 12:27:28 +0200

    Update Databricks configuration and logged model artifacts
    - Changed Databricks profile from 'marvelous' to 'dbc-4a67eabd-2dfb' in databricks.yml.
    - Updated logged model JSON with new artifact location, timestamps, metrics, and run IDs.
    - Modified MLflow experiment JSON with new experiment ID, artifact location, and owner details.
    - Updated run info JSON with new artifact URI, experiment ID, and run details.
    - Added new downloaded artifacts for logged model, experiment, and run info.
    - Refactored training and custom model registration notebooks for improved readability and consistency.
    - Fixed input logging context in basic model to correctly reflect 'testing'.
    - Added new wheel file for the marvel_characters package.

--- notebooks/lecture4.train_register_basic_model.py
+++ notebooks/lecture4.train_register_basic_model.py
@@ -1,6 +1,7 @@
 # Databricks notebook source
 
 import json
+import os
 
 import mlflow
 from dotenv import load_dotenv
@@ -8,12 +9,12 @@ from pyspark.sql import SparkSession
 
 from marvel_characters.config import ProjectConfig, Tags
 from marvel_characters.models.basic_model import BasicModel
-import os
 
 
 # Set up Databricks or local MLflow tracking
 def is_databricks():
     return "DATABRICKS_RUNTIME_VERSION" in os.environ
+
 
 # COMMAND ----------
 # If you have DEFAULT profile and are logged in with DEFAULT profile,
@@ -32,9 +33,7 @@ tags = Tags(**{"git_sha": "abcd12345", "branch": "main"})
 
 # COMMAND ----------
 # Initialize model with the config path
-basic_model = BasicModel(config=config,
-                         tags=tags,
-                         spark=spark)
+basic_model = BasicModel(config=config, tags=tags, spark=spark)
 
 # COMMAND ----------
 basic_model.load_data()
@@ -72,11 +71,11 @@ run = mlflow.get_run(basic_model.run_id)
 
 # COMMAND ----------
 inputs = run.inputs.dataset_inputs
-training_input = next((x for x in inputs if len(x.tags) > 0 and x.tags[0].value == 'training'), None)
+training_input = next((x for x in inputs if len(x.tags) > 0 and x.tags[0].value == "training"), None)
 training_source = mlflow.data.get_source(training_input)
 training_source.load()
 # COMMAND ----------
-testing_input = next((x for x in inputs if len(x.tags) > 0 and x.tags[0].value == 'testing'), None)
+testing_input = next((x for x in inputs if len(x.tags) > 0 and x.tags[0].value == "testing"), None)
 testing_source = mlflow.data.get_source(testing_input)
 testing_source.load()
 
@@ -85,11 +84,11 @@ basic_model.register_model()
 
 # COMMAND ----------
 # only searching by name is supported
-v = mlflow.search_model_versions(
-    filter_string=f"name='{basic_model.model_name}'")
+v = mlflow.search_model_versions(filter_string=f"name='{basic_model.model_name}'")
 print(v[0].__dict__)
 
 # COMMAND ----------
 # not supported
-v = mlflow.search_model_versions(
-    filter_string="tags.git_sha='abcd12345'")
+v = mlflow.search_model_versions(filter_string="tags.git_sha='abcd12345'")
+
+# COMMAND ----------
--- notebooks/lecture4.train_register_custom_model.py
+++ notebooks/lecture4.train_register_custom_model.py
@@ -1,18 +1,21 @@
 # Databricks notebook source
 
+import os
+from importlib.metadata import version
+
 import mlflow
+from dotenv import load_dotenv
+from mlflow import MlflowClient
 from pyspark.sql import SparkSession
 
 from marvel_characters.config import ProjectConfig, Tags
 from marvel_characters.models.custom_model import MarvelModelWrapper
-from importlib.metadata import version
-from dotenv import load_dotenv
-from mlflow import MlflowClient
-import os
+
 
 # Set up Databricks or local MLflow tracking
 def is_databricks():
     return "DATABRICKS_RUNTIME_VERSION" in os.environ
+
 
 # COMMAND ----------
 # If you have DEFAULT profile and are logged in with DEFAULT profile,
@@ -30,13 +33,13 @@ spark = SparkSession.builder.getOrCreate()
 tags = Tags(**{"git_sha": "abcd12345", "branch": "main"})
 marvel_characters_v = version("marvel_characters")
 
-code_paths=[f"../dist/marvel_characters-{marvel_characters_v}-py3-none-any.whl"]
+code_paths = [f"../dist/marvel_characters-{marvel_characters_v}-py3-none-any.whl"]
 
 # COMMAND ----------
 client = MlflowClient()
 wrapped_model_version = client.get_model_version_by_alias(
-    name=f"{config.catalog_name}.{config.schema_name}.marvel_character_model_basic",
-    alias="latest-model")
+    name=f"{config.catalog_name}.{config.schema_name}.marvel_character_model_basic", alias="latest-model"
+)
 # Initialize model with the config path
 
 # COMMAND ----------
@@ -46,12 +49,14 @@ X_test = test_set[config.num_features + config.cat_features]
 # COMMAND ----------
 pyfunc_model_name = f"{config.catalog_name}.{config.schema_name}.marvel_character_model_custom"
 wrapper = MarvelModelWrapper()
-wrapper.log_register_model(wrapped_model_uri=f"models:/{wrapped_model_version.model_id}",
-                           pyfunc_model_name=pyfunc_model_name,
-                           experiment_name=config.experiment_name_custom,
-                           input_example=X_test[0:1],
-                           tags=tags,
-                           code_paths=code_paths)
+wrapper.log_register_model(
+    wrapped_model_uri=f"models:/{wrapped_model_version.model_id}",
+    pyfunc_model_name=pyfunc_model_name,
+    experiment_name=config.experiment_name_custom,
+    input_example=X_test[0:1],
+    tags=tags,
+    code_paths=code_paths,
+)
 
 # COMMAND ----------
 # unwrap and predict
